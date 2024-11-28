8aca1611d5e9e4e508c06eb1619d58c84585c1a3
WideEyedTL <121069863+WideEyedTL@users.noreply.github.com>
2024-11-28 11:42:56 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+/*
+Template Name: N-GESC - Conference and Event HTML Template.
+Author: GrayGrids
+*/
+
+(function () {
+    //===== Prealoder
+
+    window.onload = function () {
+        window.setTimeout(fadeout, 500);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.querySelector(".navbar-area");
+        var sticky = header_navbar.offsetTop;
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+        } else {
+            header_navbar.classList.remove("sticky");
+        }
+        var backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+    // WOW active
+    new WOW().init();
+
+    //===== mobile-menu-btn
+    let navbarToggler = document.querySelector(".mobile-menu-btn");
+    navbarToggler.addEventListener('click', function () {
+        navbarToggler.classList.toggle("active");
+    });
+
+
+})();
+
+
+
+// Countdown
+    // Set the date we're counting down to (change this to your desired date)
+    var countDownDate = new Date("Feb 26, 2024 00:00:00").getTime();
+
+    // Update the countdown every 1 second
+    var x = setInterval(function () {
+
+        // Get the current date and time
+        var now = new Date().getTime();
+
+        // Calculate the remaining time
+        var distance = countDownDate - now;
+
+        // Calculate days, hours, minutes, and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the countdown in the respective elements
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        // If the countdown is over, display a message
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("days").innerHTML = "EXPIRED";
+            document.getElementById("hours").innerHTML = "EXPIRED";
+            document.getElementById("minutes").innerHTML = "EXPIRED";
+            document.getElementById("seconds").innerHTML = "EXPIRED";
+        }
+    }, 1000); // Update every 1 second
